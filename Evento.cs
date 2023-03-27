da75b11759b2a585be0170672cc7b5cdd96ef466
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-27 12:33:23 +0200

    milestone1

---
+++ Evento.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class Evento
+    {
+        int capienza;
+        int numeroPosti;
+        public  string Titolo { get; set; }
+        public  DateTime Data { get; set; }
+        public int Capienza
+        {
+            get
+            {
+                return capienza;
+            }
+             set { 
+                capienza = value;
+            }
+        }
+
+        public int NumeroPosti
+        {
+            get
+            {
+                return numeroPosti;
+            }
+            private set
+            {
+                numeroPosti = value;
+            }
+             
+            
+        }
+
+
+        //costruttore
+
+        public Evento(string titolo,DateTime data) {
+            Titolo = titolo;
+            Data = data;
+            Capienza = 100;
+        }
+
+        //metodi:
+
+        public int PrenotaPosti(int posti)
+        {
+            
+            this.NumeroPosti = posti;
+
+            if (this.Capienza <= this.NumeroPosti)
+            {
+                throw new Exception("non ci sono posti disponibili");
+            }else if(this.Data < DateTime.Now)
+            {
+                throw new Exception("il concerto non è più disponibile");
+            }
+            return posti;
+        }
+
+        public int DisdiciPosti(int posti)
+        {
+
+            int Decremento = this.NumeroPosti - posti;
+            
+            if ( this.NumeroPosti == 0)
+            {
+                throw new Exception("non ci sono posti da disdire");
+            }
+            else if (this.Data < DateTime.Now)
+            {
+                throw new Exception("il concerto non è più disponibile");
+            }
+            return this.numeroPosti = Decremento; ;
+        }
+
+        public override string ToString()
+        {
+            var dataformattata = Data.ToString("dd/MM/yy");
+            return $"Data:{dataformattata}, Titolo: {Titolo}";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using csharp_gestore_eventi;
+//milestone 1
+Evento concerto1 = new Evento("jazzMusic",new DateTime(2023,08,27));
+
+//Aggiunta numero di posti prenotati con conseguente salvatasggio del dato:
+Console.WriteLine(concerto1.PrenotaPosti(10));
+Console.WriteLine(concerto1.NumeroPosti);
+
+//rimozione nuemri di posti prenotati con conseguente salvatasggio del dato:
+Console.WriteLine(concerto1.DisdiciPosti(1));
+Console.WriteLine(concerto1.NumeroPosti);
+
+//stampa a schermo risultato dell'override di ToString();
+Console.WriteLine(concerto1.ToString());
